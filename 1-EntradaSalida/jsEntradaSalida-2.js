dd1b15ad67c6d75819e9393f97b0971d15c14879
LMartinez <40763533+NicaX@users.noreply.github.com>
2018-07-02 12:07:20 -0300

    Ejercicios 1 2 3
    *dab*

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,19 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	//Comentarios de una linea
+	//Tomado de internet el ej
+
+	/*
+	var person = prompt("Please enter your name", "Harry Potter");
+	resolver usando ej*/
+
+	var nombre;
+	nombre=prompt("Ingrese su Nombre");
+	alert(nombre);
+
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,27 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
+	//alert("Ejercicio 3");
+
+	//en caso de error, puedo verificar en el chrome con f12, luego en source si no lo veo en la consola
 	
+	//console.log("aver al sine");
+	
+	/*El console.log() sirve para "mensajes" entre programadores, figuran en la consola, sin 
+	embargo para poder ver los comentarios, hay que entrar en el "source"*/
+
+	/*document toma datos del html, el id lo utilizamos para leer, y para traer.
+	var nombre;
+	nombre=document.getElementById("elNombre");
+	alert(nombre);
+
+	Ese codigo nos tira un error, ya que el cuadro de texto no tiene valor, para asignarselo se usa el ".value" al final para 
+
+	*/
+	var nombre;
+	nombre=document.getElementById("elNombre").value;
+	alert(nombre);
 }
 
 
