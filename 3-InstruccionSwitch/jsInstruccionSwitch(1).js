189156292d8fbf4e6cadee5937ab82e1dd1ec750
LMartinez <40763533+NicaX@users.noreply.github.com>
2018-07-23 12:27:31 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,24 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
+	console.log(mesDelAño);
+
+	switch(mesDelAño)
+	{
+		case "Enero":
+			alert("Que comienzes bien el año!!");
+			break;
+		case  "Marzo":
+			alert("A clases!!!");
+			break;
+		case "Julio":
+			alert("Se vienen las vacaciones");
+			break;
+		case "Diciembre":
+			alert("Felices Fiestas!");
+			break;
+	}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,8 +2,28 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+ 
 
-alert (mesDelAño);
+switch(mesDelAño)
+	{
+		case "Enero":
+		case  "Febrero":
+		case "Marzo":
+		case "Abril":
+		case  "Mayo":
+		case "Junio":
+			alert("Falta para el Invierno");
+			break;
+		case "Julio":
+		case  "Agosto":
+			alert("Abrigate que hay alta rosca");
+			break;
+/*el default, es como el else, basicamente xdxd ejecuta todos los meses(casos)restantes, 
+como el usuario no tiene que ingresar nada, la condicion de deja tal como está*/
+		default:
+			alert("Ya pasamos el frio, ahora calor!");
+			break;
+	}
 
 
 
